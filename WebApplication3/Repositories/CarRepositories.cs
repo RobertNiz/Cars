3924512307e57faf86ee26009f354d1df00dd09c
ROBERT\mlody <Robert.Nizialek98@gmail.com>
2022-11-23 19:39:16 +0100

    Pierwszy projekt .Net

--- WebApplication3/Repositories/CarRepositories.cs
+++ WebApplication3/Repositories/CarRepositories.cs
@@ -1,6 +1,25 @@
-﻿namespace WebApplication3.Repositories
+﻿using System.Collections.Generic;
+using WebApplication3.Interfaces;
+
+namespace WebApplication3.Repositories
 {
-    public class CarRepositories
+    public class CarRepositories : ICarRepository
     {
+        public List<string> DeleteCars(string car)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+
+        public List<string> GetCars()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> PostCars(string car)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
